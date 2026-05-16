687a1bb3eab4e22c443f1e17bc1dcc85861f3a37
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2026-05-16 23:32:57 +0530

    Update AdminUsers.js

--- routes/AdminUsers.js
+++ routes/AdminUsers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcryptjs"); 
 const { verifyToken, verifyAdmin } = require("../middleware/authMiddleware");
 const AdminUser = require("../models/AdminUser");
 
